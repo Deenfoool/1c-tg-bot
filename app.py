f869a78529d65b9f327697e23ce0d5695e771661
Deenfoool <36488826+Deenfoool@users.noreply.github.com>
2025-06-20 11:57:34 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import http.server
+import socketserver
+import os
+
+PORT = 8000
+
+# Путь к текущей директории (где находится index.html)
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = 'index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+with socketserver.TCPServer(("", PORT), MyHttpRequestHandler) as httpd:
+    print(f"Запуск сервера на http://localhost:{PORT}")
+    print("Файлы находятся в: ", CURRENT_DIR)
+    print("Для остановки нажмите Ctrl+C")
+    httpd.serve_forever()
